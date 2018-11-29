c04983877fa767d08ad1c244bc23ab3175380664
thanhl2t <lethanhtb96@gmail.com>
2018-11-29 08:43:52 +0700

    fixbug

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,11 +1,4 @@
 class UserController < ApplicationController
-  def show
-    @user = User.find_by id: params[:id]
-    return if @user
-    flash[:danger] = t ".nouser"
-    redirect_to root_path
-  end
-
   def new
     @user = User.new
   end
@@ -14,17 +7,23 @@ class UserController < ApplicationController
     @user = User.new user_params
     if @user.save
       log_in @user
-      flash[:success] = t ".Sign_Up_Success"
+      flash[:success] = t ".success"
       redirect_to @user
     else
-      flash[:error] = t ".registration_failed"
       render :new
     end
+  end
+
+  def show
+    @user = User.find_by id: params[:id]
+    return if @user
+    flash[:danger] = t ".nouser"
+    redirect_to root_path
   end
 
   private
 
   def user_params
-    params.require(:user).permit :name, :email, :password, :confirm
+    params.require(:user).permit :user_name, :email, :password, :confirm
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   get "session/new"
-  post "/signup", to: "user#create"
+  get "users/new"
+  root "session#new"
   get "/signup", to: "user#new"
+  post "/signup", to: "user#create"
   get "/login", to: "session#new"
   post "/login", to: "session#create"
   delete "/logout", to: "session#destroy"
-  resources :user
+  get "users/show", to: "users#show"
+  resources :users
 end
